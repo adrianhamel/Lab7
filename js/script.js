4a8fb820ef25dd45b27e8d2592d71539641718de
adrianhamel <ahamel@hawk.iit.edu>
2022-11-06 03:08:47 -0600

    fixed index.html and added js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-    //add your javascrip between these two lines of code
-    
-    
-    
-    
-    
-    
-    
-    window.addEventListener('load', init);
+
+	function alertbox(){
+    var enter = document.getElementById('entryinput');
+    var output = document.getElementById('textoutput');
+
+    output.innerHTML = enter.value;
+    alert('Adrian Hamel: ' + enter.value);
 }
+  var button = document.getElementById('entrybutton');
+  button.addEventListener('click', alertbox);
+}
+window.addEventListener('load', init);
